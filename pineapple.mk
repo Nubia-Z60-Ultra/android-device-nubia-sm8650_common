5c2aa133fa6dc36e39a4d409b892bfd0e2082019
BotchedRPR <igor@chleb.me>
2024-06-08 11:54:41 +0200

    proprietary-files: deduplicate from device/qcom/common

--- pineapple.mk
+++ pineapple.mk
@@ -291,6 +291,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.secure_element-V1-ndk.vendor
 
+# ServiceTracker
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.servicetrackeraidl-V1-ndk.vendor
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service-multihal \
